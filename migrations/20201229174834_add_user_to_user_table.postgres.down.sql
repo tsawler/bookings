d900ecb2599905889a1dd8e515dc72a79d61ba41
Trevor Sawler <trevor.sawler@gmail.com>
2020-12-29 15:34:03 -0400

    Added migration to seed user table

---
+++ migrations/20201229174834_add_user_to_user_table.postgres.down.sql
@@ -0,0 +1,1 @@
+delete from users where email = 'admin@admin.com';
---
+++ migrations/20201229174834_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Trevor',E'Sawler',E'admin@admin.com',E'$2a$12$Wm8SHtNb7v9oRF6RmPP/c.PHE5tERA6mAfvShxcWJWT7i5nwXg94i',3,E'2020-12-04 00:00:00',E'2020-12-04 00:00:00');

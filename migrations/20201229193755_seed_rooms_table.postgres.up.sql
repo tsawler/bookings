06f9c7d077009fde55ee77dedf1b9304113339e8
Trevor Sawler <trevor.sawler@gmail.com>
2020-12-29 20:00:18 -0400

    Add seed for rooms

---
+++ migrations/20201229193755_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+('General''s Quarters','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+('Major''s Suite','2020-11-19 00:00:00.000','0202-11-19 00:00:00.000');

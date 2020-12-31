28c769f9e0189d1bc39a416e2f2bfc565daa4ff0
Trevor Sawler <trevor.sawler@gmail.com>
2020-12-31 12:12:52 -0400

    seeds

---
+++ migrations/20201231160553_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+('Owner Block','2020-11-19 00:00:00.000','2020-11-19 00:00:00.000');
